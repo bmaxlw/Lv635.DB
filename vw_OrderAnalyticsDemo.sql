e317978bf3559cbf84f8fccd199ce074b60266fb
Maksym Bondaruk <b.max.lw@gmail.com>
2021-10-14 14:38:21 +0300

    vw upd

--- vw_OrderAnalyticsDemo.sql
+++ vw_OrderAnalyticsDemo.sql
@@ -25,3 +25,28 @@ JOIN
 JOIN 
 	Customers c ON
 	o.CustomerID = c.CustomersID;
+
+
+-- CustomerTotal
+SELECT o.CustomerID, 
+SUM(od.TotalPrice) CustomerTotal
+FROM OrderDetails od 
+JOIN Orders o
+ON od.OrderID = o.OrderID
+GROUP BY o.CustomerID ORDER BY o.CustomerID;
+
+SELECT od.OrderID, p.CostUnit * od.Quantity FROM Products p JOIN OrderDetails od ON p.ProdID = od.ProdID;
+
+
+
+SELECT SUM(od.TotalPrice) FROM OrderDetails od JOIN Orders o ON od.OrderID = o.OrderID WHERE o.CustomerID = 1;
+SELECT * FROM Orders WHERE CustomerID = 351;
+
+SELECT TOP 10 * FROM Orders;
+SELECT * FROM OrderDetails WHERE OrderID = 818;
+SELECT * FROM Products WHERE ProdID = 559;
+select (217609.70 * 20)
+3956540.00
+-- Totals
+
+DECLARE @vw_Launcher NVARCHAR(MAX) = 'EXEC ';
