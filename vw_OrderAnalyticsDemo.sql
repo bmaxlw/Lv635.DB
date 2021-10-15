60f7c00f3c86fd16d8ac68283c73d84c6c746794
Maksym Bondaruk <b.max.lw@gmail.com>
2021-10-15 15:01:38 +0300

    vw deployed

--- vw_OrderAnalyticsDemo.sql
+++ vw_OrderAnalyticsDemo.sql
@@ -1,27 +1,59 @@
 USE Lv635_OnlineStore;
+-- =================================================================================================
+-- Author:			Maksym Bondaruk
+-- Creation date:	15.10.2021
+-- Description:		VIEW represents the number of orders made by the definite customer 
+--					as well as the number of money spent per capita
+-- =================================================================================================
 
-SELECT TOP 10 * FROM OrderDetails;
-SELECT SUM(od.TotalPrice) FROM OrderDetails od JOIN Orders o ON od.OrderID = o.OrderID WHERE o.CustomerID = 100;
-SELECT TOP 10 * FROM Customers;
-SELECT * FROM Products WHERE ProdID = 769;
-SELECT * FROM Discounts;
+CREATE VIEW vw_Orders_Counter AS
+	SELECT 
+		c.CustomersID,
+		COUNT(o.OrderID) NumberOfOrders, 
+		SUM(od.TotalPrice) + SUM(od.VAT) SpentPerCapita
+	FROM Customers c 
+		JOIN Orders o ON c.CustomersID = o.CustomerID 
+		JOIN OrderDetails od ON o.OrderID = od.OrderID
+	GROUP BY c.CustomersID;
 
--- CustomerTotal
+-- =================================================================================================
+-- Author:			Maksym Bondaruk
+-- Creation date:	15.10.2021
+-- Description:		VIEW represents the number of days, the customer is registered in our database
+-- =================================================================================================
+
+CREATE VIEW vw_CustomerRegDate_Counter AS
+	SELECT CustomersID, CustomerRegDate, 
+	DATEDIFF(day, CustomerRegDate, GETDATE()) DaysAsCustomer,
+	DATEDIFF(month, CustomerRegDate, GETDATE()) MonthsAsCustomer,
+	DATEDIFF(year, CustomerRegDate, GETDATE()) YearsAsCustomer
+	FROM Customers;
+
+-- =================================================================================================
+-- Author:			Maksym Bondaruk
+-- Creation date:	15.10.2021
+-- Description:		VIEW represents brief overview on the customers' activity in the online shop. 
+--					- Columns Days/Months/Years represent the number of days/months/years after RegDate
+--					- Columns OrdersPerMonth/OrdersPerYear show the avg of orders per a certain period of time
+--					- Columns SpentPerMonth/SpentPerYear show the number of money spent by a definite customer 
+--						per a certain period of time
+--					- TotalSpent shows how much money has the definite customer spent since the RegDate
+-- =================================================================================================
+CREATE VIEW vw_Customer_Analysis AS
 SELECT 
-	o.CustomerID, 
-	COUNT(od.OrderID) OrdersTotal, --> The total number of orders, made by one customer
-	SUM(od.TotalPrice + od.VAT) CustomerTotal --> The total of money, spent by the definite customer (VAT included)
-FROM OrderDetails od
-JOIN Orders o ON od.OrderID = o.OrderID 
-WHERE o.CustomerID > 0 
-GROUP BY o.CustomerID 
-ORDER BY o.CustomerID;
-
--- Discount is applied to TotalPrice + VAT
--- Final Price, to be paid by customer, is actually TotalPrice + VAT + Discount
--- Thus the discount also directly affects the VAT and seller is intrested in setting discounts
--- EXAMPLE
--- Total Price:		1000 UAH
--- VAT:				 200 UAH (1000 * 0.2)
--- Discount (5%):	  60 UAH (1200 * 0.05)
--- To be paid:		1140 UAH
+	vwa.CustomersID CustomerID, 
+	CONCAT(c.CustomerFirstName, ' ', c.CustomerLastName) CustomerName,
+	c.CustomerEmail Email,
+	vwb.CustomerRegDate RegDate,
+	vwb.DaysAsCustomer [Days],
+	vwb.MonthsAsCustomer [Months],
+	vwb.YearsAsCustomer [Years],
+	vwa.NumberOfOrders Orders,
+	vwa.NumberOfOrders / vwb.MonthsAsCustomer OrdersPerMonth,
+	vwa.NumberOfOrders / vwb.YearsAsCustomer OrdersPerYear,
+	ROUND((vwa.SpentPerCapita / MonthsAsCustomer), 2) SpentPerMonth,
+	ROUND((vwa.SpentPerCapita / YearsAsCustomer), 2) SpentPerYear,
+	vwa.SpentPerCapita TotalSpent
+FROM vw_Orders_Counter vwa 
+	JOIN vw_CustomerRegDate_Counter vwb ON vwa.CustomersID = vwb.CustomersID
+	JOIN Customers c ON vwa.CustomersID = c.CustomersID;
