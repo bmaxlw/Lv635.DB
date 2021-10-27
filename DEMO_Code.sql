3f6304fa9ab78ea74a65c98d69db81fa35f39ce8
Maksym Bondaruk <b.max.lw@gmail.com>
2021-10-27 22:03:44 +0300

    ...

--- DEMO_Code.sql
+++ DEMO_Code.sql
@@ -1,12 +1,12 @@
 USE Lv635_OnlineStore;
 
 -- trg_NewOrderToOrderDetails_INS
-EXEC spr_GenerateNewOrder
+EXEC spr_GenerateNewOrder;
 SELECT TOP 5 OrderID, OrderDate, CustomerID, ShippingAddress, ShippingDate FROM Orders ORDER BY OrderID DESC;
 SELECT TOP 5 * FROM OrderDetails ORDER BY OrderDetailsID DESC;
 
 -- spr_NewOrderDetails
-SELECT TOP 5 * FROM Products ORDER BY QtInStock DESC;
+SELECT TOP 10 * FROM Products ORDER BY QtInStock DESC;
 EXEC spr_NewOrderDetails 4999985, 532, 2;   
 SELECT TOP 5 * FROM OrderDetails ORDER BY OrderDetailsID DESC;
 SELECT (227378.68 * 2)
@@ -20,11 +20,11 @@ SELECT * FROM Discounts;
 SELECT TOP 5 * FROM OrderDetails ORDER BY OrderDetailsID DESC;
 
 -- spr_ShowProfits
-EXEC spr_ShowProfits '2021-10-25', '2021-10-26';
-SELECT OrderID, OrderDate FROM Orders WHERE OrderDate IN ('2021-10-25', '2021-10-26');
+EXEC spr_ShowProfits '2021-10-25', '2021-10-25';
+SELECT OrderID, OrderDate FROM Orders WHERE OrderDate IN ('2021-10-25', '2021-10-25');
 
 -- fn_QtChecker
-SELECT * FROM fn_QtChecker(0, 1);
+SELECT * FROM fn_QtChecker(0, 10);
 
 -- vw_Orders_Counter + vw_CustomerRegDate_Counter + 
 SELECT * FROM vw_Orders_Counter;
