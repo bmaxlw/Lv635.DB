73f8be9622c472175f7d1326cbf01ff468fa2084
Maksym Bondaruk <b.max.lw@gmail.com>
2021-11-16 23:37:01 +0200

    d2

--- demo2/dwh_solution.sql
+++ demo2/dwh_solution.sql
@@ -1,154 +1,76 @@
---USE Lv635_Staging
-
---WITH MergeCTE(
---OrderID,
---OrderDetailsID,
---OrderDate,
---ShippingDate,
---CustomerID,
---ProdID,
---Quantity,
---VAT,
---TotalPrice,
---ShippingAddress,
---ShippingID,
---DiscountID,
---PaymentMethod,
---WarrantyStartDate,
---WarrantyExpDate,
---AssignedTo) AS
---(SELECT 
---OrderID,
---OrderDetailsID,
---OrderDate,
---ShippingDate,
---CustomerID,
---ProdID,
---Quantity,
---VAT,
---TotalPrice,
---ShippingAddress,
---ShippingID,
---DiscountID,
---PaymentMethod,
---WarrantyStartDate,
---WarrantyExpDate,
---AssignedTo
---FROM 
---dbo.FactOrders AS src)
---MERGE [Lv635_DataWarehouse].dbo.FactOrders AS dst USING (dbo.FactOrders AS src) ON (src.OrderID = dst.OrderID)
-
-
-MERGE INTO [Lv635_DataWarehouse].dbo.FactOrders DST
-USING [Lv635_Staging].dbo.FactOrders SRC
-ON (
-       SRC.OrderID = DST.OrderID
-       AND SRC.OrderDetailsID = DST.OrderDetailsID
-       --OR SRC.OrderDate = DST.OrderDate
-       --OR SRC.ShippingDate = DST.ShippingDate
-       --OR SRC.CustomerID = DST.CustomerID
-       --OR SRC.ProdID = DST.ProdID
-       --OR SRC.Quantity = DST.Quantity
-       --OR SRC.VAT = DST.VAT
-       --OR SRC.TotalPrice = DST.TotalPrice
-       --OR SRC.ShippingAddress = DST.ShippingAddress
-       --OR SRC.ShippingID = DST.ShippingID
-       --OR SRC.DiscountID = DST.DiscountID
-       --OR SRC.PaymentMethod = DST.PaymentMethod
-       --OR SRC.WarrantyStartDate = DST.WarrantyStartDate
-       --OR SRC.WarrantyExpDate = DST.WarrantyExpDate
-       --OR SRC.AssignedTo = DST.AssignedTo
-   )
-WHEN MATCHED THEN
-    UPDATE SET DST.OrderID = SRC.OrderID,
-               DST.OrderDetailsID = SRC.OrderDetailsID,
-               OrderDate = SRC.OrderDate,
-               ShippingDate = SRC.ShippingDate,
-               CustomerID = SRC.CustomerID,
-               ProdID = SRC.ProdID,
-               Quantity = SRC.Quantity,
-               VAT = SRC.VAT,
-               TotalPrice = SRC.TotalPrice,
-               ShippingAddress = SRC.ShippingAddress,
-               ShippingID = SRC.ShippingID,
-               DiscountID = SRC.DiscountID,
-               PaymentMethod = SRC.PaymentMethod,
-               WarrantyStartDate = SRC.WarrantyStartDate,
-               WarrantyExpDate = SRC.WarrantyExpDate,
-               AssignedTo = SRC.AssignedTo
-WHEN NOT MATCHED THEN
-    INSERT
-    (
-        OrderID,
-        OrderDetailsID,
-        OrderDate,
-        ShippingDate,
-        CustomerID,
-        ProdID,
-        Quantity,
-        VAT,
-        TotalPrice,
-        ShippingAddress,
-        ShippingID,
-        DiscountID,
-        PaymentMethod,
-        WarrantyStartDate,
-        WarrantyExpDate,
-        AssignedTo
-    )
-    VALUES
-    (SRC.OrderID,
-     SRC.OrderDetailsID,
-     SRC.OrderDate,
-     SRC.ShippingDate,
-     SRC.CustomerID,
-     SRC.ProdID,
-     SRC.Quantity,
-     SRC.VAT,
-     SRC.TotalPrice,
-     SRC.ShippingAddress,
-     SRC.ShippingID,
-     SRC.DiscountID,
-     SRC.PaymentMethod,
-     SRC.WarrantyStartDate,
-     SRC.WarrantyExpDate,
-     SRC.AssignedTo
-    );
---WHEN NOT MATCHED BY SOURCE THEN
---	INSERT
---    (
---        OrderID,
---        OrderDetailsID,
---        OrderDate,
---        ShippingDate,
---        CustomerID,
---        ProdID,
---        Quantity,
---        VAT,
---        TotalPrice,
---        ShippingAddress,
---        ShippingID,
---        DiscountID,
---        PaymentMethod,
---        WarrantyStartDate,
---        WarrantyExpDate,
---        AssignedTo
---    )
---    VALUES
---    (SRC.OrderID,
---     SRC.OrderDetailsID,
---     SRC.OrderDate,
---     SRC.ShippingDate,
---     SRC.CustomerID,
---     SRC.ProdID,
---     SRC.Quantity,
---     SRC.VAT,
---     SRC.TotalPrice,
---     SRC.ShippingAddress,
---     SRC.ShippingID,
---     SRC.DiscountID,
---     SRC.PaymentMethod,
---     SRC.WarrantyStartDate,
---     SRC.WarrantyExpDate,
---     SRC.AssignedTo
---    );
+-- =================================================================================================
+-- Author:			Maksym Bondaruk
+-- Creation date:	16.11.2021
+-- Last update:		16.11.2021
+-- Description:		Migration of clean data to DWH with merging
+-- =================================================================================================
+CREATE PROCEDURE spr_OrdersMigrationDWH
+AS
+BEGIN TRY
+    EXEC [Lv635_OnlineStore].[dbo].[spr_LogProc] @ProcessName = '[MBLV635DB]: spr_OrdersMigrationDWH',
+                                                 @LevelProcedure = 'START'
+    SET NOCOUNT ON;
+    SET ANSI_WARNINGS OFF;
+    MERGE INTO [Lv635_DataWarehouse].dbo.FactOrders DST
+    USING [Lv635_Staging].dbo.FactOrders SRC
+       ON (   SRC.OrderID = DST.OrderID
+        AND   SRC.OrderDetailsID = DST.OrderDetailsID)
+     WHEN MATCHED THEN UPDATE SET DST.OrderID = SRC.OrderID,
+                                  DST.OrderDetailsID = SRC.OrderDetailsID,
+                                  OrderDate = SRC.OrderDate,
+                                  ShippingDate = SRC.ShippingDate,
+                                  CustomerID = SRC.CustomerID,
+                                  ProdID = SRC.ProdID,
+                                  Quantity = SRC.Quantity,
+                                  VAT = SRC.VAT,
+                                  TotalPrice = SRC.TotalPrice,
+                                  ShippingAddress = SRC.ShippingAddress,
+                                  ShippingID = SRC.ShippingID,
+                                  DiscountID = SRC.DiscountID,
+                                  PaymentMethod = SRC.PaymentMethod,
+                                  WarrantyStartDate = SRC.WarrantyStartDate,
+                                  WarrantyExpDate = SRC.WarrantyExpDate,
+                                  AssignedTo = SRC.AssignedTo
+     WHEN NOT MATCHED THEN
+        INSERT (OrderID,
+                OrderDetailsID,
+                OrderDate,
+                ShippingDate,
+                CustomerID,
+                ProdID,
+                Quantity,
+                VAT,
+                TotalPrice,
+                ShippingAddress,
+                ShippingID,
+                DiscountID,
+                PaymentMethod,
+                WarrantyStartDate,
+                WarrantyExpDate,
+                AssignedTo)
+        VALUES (SRC.OrderID,
+                SRC.OrderDetailsID,
+                SRC.OrderDate,
+                SRC.ShippingDate,
+                SRC.CustomerID,
+                SRC.ProdID,
+                SRC.Quantity,
+                SRC.VAT,
+                SRC.TotalPrice,
+                SRC.ShippingAddress,
+                SRC.ShippingID,
+                SRC.DiscountID,
+                SRC.PaymentMethod,
+                SRC.WarrantyStartDate,
+                SRC.WarrantyExpDate,
+                SRC.AssignedTo);
+    EXEC [Lv635_OnlineStore].[dbo].[spr_LogProc] @ProcessName = '[MBLV635DB]: spr_OrdersMigrationDWH',
+                                                 @LevelProcedure = 'STOP'
+END TRY
+BEGIN CATCH
+    DECLARE @Error_message NVARCHAR(100)
+    SET @Error_message = (SELECT ERROR_MESSAGE())
+    EXEC [Lv635_OnlineStore].[dbo].[spr_LogProc] @ProcessName = '[MBLV635DB]: spr_OrdersMigrationDWH',
+                                                 @LevelProcedure = 'FATAL',
+                                                 @Context = @Error_message
+END CATCH;
--- demo2/maintanance.sql
+++ demo2/maintanance.sql
@@ -1,27 +1,142 @@
-USE Lv635_Staging;
+BEGIN
+CREATE TABLE ##Composite(
+Identificator INT IDENTITY(1, 1),
+CompositeKey INT,
+OrderID INT NOT NULL,
+OrderDetailsID INT NOT NULL,
+OrderDate DATE,
+ShippingDate DATE,
+CustomerID INT,
+ProdID INT,
+Quantity INT,
+VAT NUMERIC(19, 2),
+TotalPrice NUMERIC(19, 2),
+ShippingAddress NVARCHAR(70),
+ShippingID INT,
+DiscountID INT,
+PaymentMethod NVARCHAR(70),
+WarrantyStartDate DATE,
+WarrantyExpDate DATE,
+AssignedTo INT);
+
+INSERT INTO ##Composite(
+									   CompositeKey,
+                                       OrderID,
+                                       OrderDetailsID,
+                                       OrderDate,
+                                       ShippingDate,
+                                       CustomerID,
+                                       ProdID,
+                                       Quantity,
+                                       VAT,
+                                       TotalPrice,
+                                       ShippingAddress,
+                                       ShippingID,
+                                       DiscountID,
+                                       PaymentMethod,
+                                       WarrantyStartDate,
+                                       WarrantyExpDate,
+                                       AssignedTo)
+        SELECT 
+			   CAST(CONCAT(OrderID, OrderDetailsID) AS INT),
+               CAST(OrderID AS INT),
+               CAST(OrderDetailsID AS INT),
+               CAST(OrderDate AS DATE),
+               CAST(ShippingDate AS DATE),
+               CAST(CustomerID AS INT),
+               CAST(ProdID AS INT),
+               CAST(Quantity AS INT),
+               CAST(VAT AS NUMERIC(19,2)),
+               CAST(TotalPrice AS NUMERIC(19,2)),
+               CAST(ShippingAddress AS NVARCHAR(70)),
+               CAST(ShippingID AS INT),
+               CAST(DiscountID AS INT), 
+               CAST(PaymentMethod AS NVARCHAR(50)),
+               CAST(WarrantyStartDate AS DATE),
+               CAST(WarrantyExpDate AS DATE),
+               AssignedTo
+          FROM stg.FactOrders
+         WHERE OrderID IN (   SELECT OrderID
+                                FROM stg.FactOrders
+                               GROUP BY OrderID
+                              HAVING COUNT(OrderID) > 1 );
 
 
-SELECT * FROM stg.FactOrders;
-SELECT * FROM dbo.FactOrders;
-SELECT * FROM stg.ErrorOrders;
+WHILE ((SELECT MIN(Identificator) FROM ##Composite) IS NOT NULL)
+BEGIN
+DECLARE @Ckey INT = (SELECT MIN(CompositeKey) FROM ##Composite);
+DECLARE @MinID INT = (SELECT MIN(Identificator) FROM ##Composite WHERE CompositeKey = @Ckey);
+DECLARE @MaxID INT = (SELECT MAX(Identificator) FROM ##Composite WHERE CompositeKey = @Ckey);
 
-EXEC sp_help FactOrders;
+INSERT INTO [dbo].FactOrders (OrderID,
+                                      OrderDetailsID,
+                                      OrderDate,
+                                      ShippingDate,
+                                      CustomerID,
+                                      ProdID,
+                                      Quantity,
+                                      VAT,
+                                      TotalPrice,
+                                      ShippingAddress,
+                                      ShippingID,
+                                      DiscountID,
+                                      PaymentMethod,
+                                      WarrantyStartDate,
+                                      WarrantyExpDate,
+                                      AssignedTo)
+SELECT OrderID, OrderDetailsID, OrderDate, ShippingDate, CustomerID, ProdID, Quantity,
+                                      VAT,
+                                      TotalPrice,
+                                      ShippingAddress,
+                                      ShippingID,
+                                      DiscountID,
+                                      PaymentMethod,
+                                      WarrantyStartDate,
+                                      WarrantyExpDate,
+                                      AssignedTo FROM ##Composite 
+WHERE CompositeKey = @Ckey AND Identificator = @MinID;
 
-TRUNCATE TABLE stg.FactOrders;
-TRUNCATE TABLE dbo.FactOrders;
-TRUNCATE TABLE stg.ErrorOrders;
+DELETE FROM ##Composite WHERE CompositeKey = @Ckey AND Identificator = @MinID;
 
-INSERT INTO stg.FactOrders VALUES('1', '1', '2021-01-01', '2021-01-01', '1', '1', '1', '1.00', '1.00', 'Street', '1', '1', 'Cash', '2021-01-01', '2021-01-01', '1');
-INSERT INTO stg.FactOrders VALUES('2', '1', '2021-01-01', '2021-01-01', '1', '1', '1', '1.00', '1.00', 'Street', '1', '1', 'Cash', '2021-01-01', '2021-01-01', '1');
-INSERT INTO stg.FactOrders VALUES('1', '1', '2021-01-01', '2021-01-01', '1', '1', '1', '1.00', '1.00', 'Street', '1', '1', 'Cash', '2021-01-01', '2021-01-01', '1');
-INSERT INTO stg.FactOrders VALUES('xx', '1', '2021-01-01', '2021-01-01', '1', '1', '1', '1.00', '1.00', 'Street', '1', '1', 'Cash', '2021-01-01', '2021-01-01', '1');
-INSERT INTO stg.FactOrders VALUES('3', '1', '2013-01-01', '2021-01-01', '1', '1', '1', '1.00', '1.00', 'Street', '1', '1', 'Cash', '2021-01-01', '2021-01-01', '1');
-INSERT INTO stg.FactOrders VALUES('4', '1', '2021-01-01', '2021-01-01', '1', '1', '1', '1.00', '1.00', 'Street', '1', '2147483648', 'Cash', '2021-01-01', '2021-01-01', '1');
+INSERT INTO [stg].ErrorOrders (ErrorDescription,
+                                       OrderID,
+                                       OrderDetailsID,
+                                       OrderDate,
+                                       ShippingDate,
+                                       CustomerID,
+                                       ProdID,
+                                       Quantity,
+                                       VAT,
+                                       TotalPrice,
+                                       ShippingAddress,
+                                       ShippingID,
+                                       DiscountID,
+                                       PaymentMethod,
+                                       WarrantyStartDate,
+                                       WarrantyExpDate,
+                                       AssignedTo)
+        SELECT 'Duplicated values assigned for PKEYs',
+               OrderID,
+               OrderDetailsID,
+               OrderDate,
+               ShippingDate,
+               CustomerID,
+               ProdID,
+               Quantity,
+               VAT,
+               TotalPrice,
+               ShippingAddress,
+               ShippingID,
+               DiscountID,
+               PaymentMethod,
+               WarrantyStartDate,
+               WarrantyExpDate,
+               AssignedTo 
+		FROM ##Composite
+		where CompositeKey = @Ckey;
 
-DROP PROC spr_DiscardConversionIssues_Orders; --> FIRST
-DROP PROC spr_DiscardDuplicates_Orders; --> SECOND
-DROP PROC spr_DiscardIncorrectValues_Orders; --> THIRD
-DROP PROC spr_Conversion_Orders; --> FOURTH;
-DROP PROC spr_CleanStgOrders;
-
-EXEC spr_CleanStpOrders;
+DELETE FROM ##Composite WHERE CompositeKey = @Ckey;
+		
+		END
+DROP TABLE ##Composite
+END
