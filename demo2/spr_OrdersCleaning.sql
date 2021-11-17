037ea1d2d8174b9408f72c6222e9c90f4271e26b
Maksym Bondaruk <b.max.lw@gmail.com>
2021-11-17 11:27:36 +0200

    v2

--- demo2/spr_OrdersCleaning.sql
+++ demo2/spr_OrdersCleaning.sql
@@ -5,7 +5,7 @@
 -- Description:		Cleaning and migration from stg.FactOrders to dbo.FactOrders
 -- =================================================================================================
 
--- STAGE 1/4. Discard conversion issues from the raw data
+-- STAGE 1/3. Discard conversion issues from the raw data
 CREATE PROCEDURE spr_OrdersCleaning
 AS
 SET NOCOUNT ON;
@@ -69,159 +69,8 @@ BEGIN TRY
          WHERE OrderID IN ( SELECT OrderID FROM stg.ErrorOrders );
     END
 
-    -- STAGE 2/4. Discard duplicates from the raw data.
-    BEGIN
-        CREATE TABLE ##Composite (
-            Identificator INT IDENTITY(1, 1),
-            CompositeKey INT,
-            OrderID INT NOT NULL,
-            OrderDetailsID INT NOT NULL,
-            OrderDate DATE,
-            ShippingDate DATE,
-            CustomerID INT,
-            ProdID INT,
-            Quantity INT,
-            VAT NUMERIC(19, 2),
-            TotalPrice NUMERIC(19, 2),
-            ShippingAddress NVARCHAR(70),
-            ShippingID INT,
-            DiscountID INT,
-            PaymentMethod NVARCHAR(70),
-            WarrantyStartDate DATE,
-            WarrantyExpDate DATE,
-            AssignedTo INT);
 
-        INSERT INTO ##Composite (CompositeKey,
-                                 OrderID,
-                                 OrderDetailsID,
-                                 OrderDate,
-                                 ShippingDate,
-                                 CustomerID,
-                                 ProdID,
-                                 Quantity,
-                                 VAT,
-                                 TotalPrice,
-                                 ShippingAddress,
-                                 ShippingID,
-                                 DiscountID,
-                                 PaymentMethod,
-                                 WarrantyStartDate,
-                                 WarrantyExpDate,
-                                 AssignedTo)
-        SELECT CAST(CONCAT(OrderID, OrderDetailsID) AS INT),
-               CAST(OrderID AS INT),
-               CAST(OrderDetailsID AS INT),
-               CAST(OrderDate AS DATE),
-               CAST(ShippingDate AS DATE),
-               CAST(CustomerID AS INT),
-               CAST(ProdID AS INT),
-               CAST(Quantity AS INT),
-               CAST(VAT AS NUMERIC(19, 2)),
-               CAST(TotalPrice AS NUMERIC(19, 2)),
-               CAST(ShippingAddress AS NVARCHAR(70)),
-               CAST(ShippingID AS INT),
-               CAST(DiscountID AS INT),
-               CAST(PaymentMethod AS NVARCHAR(50)),
-               CAST(WarrantyStartDate AS DATE),
-               CAST(WarrantyExpDate AS DATE),
-               AssignedTo
-          FROM stg.FactOrders
-         WHERE OrderID IN (   SELECT OrderID
-                                FROM stg.FactOrders
-                               GROUP BY OrderID
-                              HAVING COUNT(OrderID) > 1 );
-
-
-        WHILE ((SELECT MIN(Identificator) FROM ##Composite) IS NOT NULL)
-        BEGIN
-            DECLARE @Ckey INT = (SELECT MIN(CompositeKey) FROM ##Composite);
-            DECLARE @MinID INT = (SELECT MIN(Identificator) FROM ##Composite WHERE CompositeKey = @Ckey);
-
-            INSERT INTO [dbo].FactOrders (OrderID,
-                                          OrderDetailsID,
-                                          OrderDate,
-                                          ShippingDate,
-                                          CustomerID,
-                                          ProdID,
-                                          Quantity,
-                                          VAT,
-                                          TotalPrice,
-                                          ShippingAddress,
-                                          ShippingID,
-                                          DiscountID,
-                                          PaymentMethod,
-                                          WarrantyStartDate,
-                                          WarrantyExpDate,
-                                          AssignedTo)
-            SELECT OrderID,
-                   OrderDetailsID,
-                   OrderDate,
-                   ShippingDate,
-                   CustomerID,
-                   ProdID,
-                   Quantity,
-                   VAT,
-                   TotalPrice,
-                   ShippingAddress,
-                   ShippingID,
-                   DiscountID,
-                   PaymentMethod,
-                   WarrantyStartDate,
-                   WarrantyExpDate,
-                   AssignedTo
-              FROM ##Composite
-             WHERE CompositeKey  = @Ckey
-               AND Identificator = @MinID;
-
-            DELETE FROM ##Composite
-             WHERE CompositeKey  = @Ckey
-               AND Identificator = @MinID;
-
-            INSERT INTO [stg].ErrorOrders (ErrorDescription,
-                                           OrderID,
-                                           OrderDetailsID,
-                                           OrderDate,
-                                           ShippingDate,
-                                           CustomerID,
-                                           ProdID,
-                                           Quantity,
-                                           VAT,
-                                           TotalPrice,
-                                           ShippingAddress,
-                                           ShippingID,
-                                           DiscountID,
-                                           PaymentMethod,
-                                           WarrantyStartDate,
-                                           WarrantyExpDate,
-                                           AssignedTo)
-            SELECT '[Error 2]: Duplicated values assigned for PKEYs',
-                   OrderID,
-                   OrderDetailsID,
-                   OrderDate,
-                   ShippingDate,
-                   CustomerID,
-                   ProdID,
-                   Quantity,
-                   VAT,
-                   TotalPrice,
-                   ShippingAddress,
-                   ShippingID,
-                   DiscountID,
-                   PaymentMethod,
-                   WarrantyStartDate,
-                   WarrantyExpDate,
-                   AssignedTo
-              FROM ##Composite
-             where CompositeKey = @Ckey;
-
-            DELETE FROM ##Composite
-             WHERE CompositeKey = @Ckey;
-
-        END
-        DROP TABLE ##Composite
-    END
-
-    -- STAGE 3/4. Discards incorrect values from the raw data.
+    -- STAGE 2/3. Discards incorrect values from the raw data.
     BEGIN
         DECLARE @LenShippingAddress INT = (   SELECT CHARACTER_MAXIMUM_LENGTH
                                                 FROM INFORMATION_SCHEMA.COLUMNS
@@ -260,7 +109,7 @@ BEGIN TRY
                                        WarrantyStartDate,
                                        WarrantyExpDate,
                                        AssignedTo)
-        SELECT '[Error 3]: Date/Length Formatting violated',
+        SELECT '[Error 2]: DATE formatting and/or value length violated',
                OrderID,
                OrderDetailsID,
                OrderDate,
@@ -290,42 +139,136 @@ BEGIN TRY
          WHERE OrderID IN ( SELECT OrderID FROM stg.ErrorOrders );
     END
 
-    -- STAGE 4/4. Maintain conversion.
+    -- STAGE 3/3. DISCARD DUPLICATES ASSIGNED TO COMPOSITE PRIMARY KEYS AND MAINTAIN FINAL CONVERSION WITH MIGRATION OF CLEAN DATA
     BEGIN
-        INSERT INTO [dbo].FactOrders (OrderID,
-                                      OrderDetailsID,
-                                      OrderDate,
-                                      ShippingDate,
-                                      CustomerID,
-                                      ProdID,
-                                      Quantity,
-                                      VAT,
-                                      TotalPrice,
-                                      ShippingAddress,
-                                      ShippingID,
-                                      DiscountID,
-                                      PaymentMethod,
-                                      WarrantyStartDate,
-                                      WarrantyExpDate,
-                                      AssignedTo)
-        SELECT CAST(OrderID AS INT),
-               CAST(OrderDetailsID AS INT),
-               CAST(OrderDate AS DATE),
-               CAST(ShippingDate AS DATE),
-               CAST(CustomerID AS INT),
-               CAST(ProdID AS INT),
-               CAST(Quantity AS INT),
-               CAST(VAT AS NUMERIC(19, 2)),
-               CAST(TotalPrice AS NUMERIC(19, 2)),
-               CAST(ShippingAddress AS NVARCHAR(100)),
-               CAST(ShippingID AS INT),
-               CAST(DiscountID AS INT),
-               CAST(PaymentMethod AS NVARCHAR(50)),
-               CAST(WarrantyStartDate AS DATE),
-               CAST(WarrantyExpDate AS DATE),
-               CAST(AssignedTo AS INT)
-          FROM stg.FactOrders;
+
+		WITH ClnCTE (Rnk, Ckey, OrderID, OrderDetailsID, OrderDate, ShippingDate, CustomerID, ProdID, Quantity, VAT,
+             TotalPrice, ShippingAddress, ShippingID, DiscountID, PaymentMethod, WarrantyStartDate, WarrantyExpDate,
+             AssignedTo
+            )
+AS (SELECT ROW_NUMBER() OVER (PARTITION BY (OrderID + OrderDetailsID) ORDER BY OrderID) Rnk,
+           (OrderID + OrderDetailsID) Ckey,
+           OrderID,
+           OrderDetailsID,
+           OrderDate,
+           ShippingDate,
+           CustomerID,
+           ProdID,
+           Quantity,
+           VAT,
+           TotalPrice,
+           ShippingAddress,
+           ShippingID,
+           DiscountID,
+           PaymentMethod,
+           WarrantyStartDate,
+           WarrantyExpDate,
+           AssignedTo
+    FROM stg.FactOrders
+   )
+INSERT INTO dbo.FactOrders
+(
+    OrderID,
+    OrderDetailsID,
+    OrderDate,
+    ShippingDate,
+    CustomerID,
+    ProdID,
+    Quantity,
+    VAT,
+    TotalPrice,
+    ShippingAddress,
+    ShippingID,
+    DiscountID,
+    PaymentMethod,
+    WarrantyStartDate,
+    WarrantyExpDate,
+    AssignedTo
+)
+SELECT TRY_CAST(OrderID AS INT),
+       TRY_CAST(OrderDetailsID AS INT),
+       TRY_CAST(OrderDate AS DATE),
+       TRY_CAST(ShippingDate AS DATE),
+       TRY_CAST(CustomerID AS INT),
+       TRY_CAST(ProdID AS INT),
+       TRY_CAST(Quantity AS INT),
+       TRY_CAST(VAT AS NUMERIC(19, 2)),
+       TRY_CAST(TotalPrice AS NUMERIC(19, 2)),
+       TRY_CAST(ShippingAddress AS NVARCHAR(100)),
+       TRY_CAST(ShippingID AS INT),
+       TRY_CAST(DiscountID AS INT),
+       TRY_CAST(PaymentMethod AS NVARCHAR(50)),
+       TRY_CAST(WarrantyStartDate AS DATE),
+       TRY_CAST(WarrantyExpDate AS DATE),
+       TRY_CAST(AssignedTo AS INT)
+FROM ClnCTE
+WHERE Rnk = 1;
+
+WITH ErrCTE (Rnk, Ckey, OrderID, OrderDetailsID, OrderDate, ShippingDate, CustomerID, ProdID, Quantity, VAT,
+             TotalPrice, ShippingAddress, ShippingID, DiscountID, PaymentMethod, WarrantyStartDate, WarrantyExpDate,
+             AssignedTo
+            )
+AS (SELECT ROW_NUMBER() OVER (PARTITION BY (OrderID + OrderDetailsID) ORDER BY OrderID) Rnk,
+           (OrderID + OrderDetailsID) Ckey,
+           OrderID,
+           OrderDetailsID,
+           OrderDate,
+           ShippingDate,
+           CustomerID,
+           ProdID,
+           Quantity,
+           VAT,
+           TotalPrice,
+           ShippingAddress,
+           ShippingID,
+           DiscountID,
+           PaymentMethod,
+           WarrantyStartDate,
+           WarrantyExpDate,
+           AssignedTo
+    FROM stg.FactOrders
+   )
+INSERT INTO stg.ErrorOrders
+(
+    ErrorDescription,
+    OrderID,
+    OrderDetailsID,
+    OrderDate,
+    ShippingDate,
+    CustomerID,
+    ProdID,
+    Quantity,
+    VAT,
+    TotalPrice,
+    ShippingAddress,
+    ShippingID,
+    DiscountID,
+    PaymentMethod,
+    WarrantyStartDate,
+    WarrantyExpDate,
+    AssignedTo
+)
+SELECT '[Error 3]: Duplicates assigned for Primary keys',
+       OrderID,
+       OrderDetailsID,
+       OrderDate,
+       ShippingDate,
+       CustomerID,
+       ProdID,
+       Quantity,
+       VAT,
+       TotalPrice,
+       ShippingAddress,
+       ShippingID,
+       DiscountID,
+       PaymentMethod,
+       WarrantyStartDate,
+       WarrantyExpDate,
+       AssignedTo
+FROM ErrCTE
+WHERE Rnk > 1;
         TRUNCATE TABLE stg.FactOrders;
+
     END
     EXEC [Lv635_OnlineStore].[dbo].[spr_LogProc] @ProcessName = '[MBLV635DB]: spr_OrdersCleaning',
                                                  @LevelProcedure = 'STOP'
